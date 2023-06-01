5f84a981b93690aa6545e865bd39473e666c1cc8
Hakan Celik <hakancelikdev@gmail.com>
2023-06-01 19:10:51 +0000

    Update Docs

---
+++ examples/auto_slots.py
@@ -0,0 +1,23 @@
+class Meta(type):
+    def __new__(mcs, name, bases, namespace, **kwargs):
+        __slots__ = namespace.get("__slots__", ())
+        if not __slots__:
+            if __init__ := namespace.get("__init__", None):
+                __slots__ = __init__.__code__.co_names
+                namespace["__slots__"] = __slots__
+        return super().__new__(mcs, name, bases, namespace, **kwargs)
+
+
+class AutoSlots(metaclass=Meta):
+    pass
+
+
+class Example(AutoSlots):
+    def __init__(self, field: str, *, keyword_field: int):
+        self.field = field
+        self.keyword_field = keyword_field
+        self.constant = "constant"
+
+
+assert Example.__slots__ == ("field", "keyword_field", "constant")
+assert Example("field", keyword_field=1).__dict__ == {}
---
+++ examples/modeling_a_class_with_a_metaclass.py
@@ -0,0 +1,46 @@
+class Meta(type):
+    def __new__(mcs, name, bases, namespace, **kwargs):
+        if not [base for base in bases if isinstance(base, mcs)]:
+            return super().__new__(mcs, name, bases, namespace, **kwargs)
+
+        annotations = namespace.get('__annotations__', {})
+        namespace["__slots__"] = tuple(annotations.keys())  # auto __slots__
+
+        return super().__new__(mcs, name, bases, namespace, **kwargs)
+
+    def __call__(cls, **kwargs):
+        for field_name, field_type in cls.__annotations__.items():
+            if field_name not in kwargs:
+                raise ValueError(f"Missing field {field_name}")
+            else:
+                assert isinstance(kwargs[field_name], field_type), f"Field {field_name} must be of type {field_type}"
+
+        for field_name, field_value in kwargs.items():
+            if field_name not in cls.__annotations__:
+                raise ValueError(f"Unknown field {field_name}")
+
+        return super().__call__(**kwargs)
+
+
+class BaseModel(metaclass=Meta):
+    def __init__(self, **kwargs):
+        for field_name, field_value in kwargs.items():
+            setattr(self, field_name, field_value)
+
+
+class Account(BaseModel):
+    first_name: str
+    last_name: str
+    username: str
+
+
+account = Account(first_name="Hakan", last_name="Çelik", username="hakancelik")
+
+assert account.__slots__ == ("first_name", "last_name", "username")
+assert account.__dict__ == {}
+
+assert account.first_name == "Hakan"
+assert account.last_name == "Çelik"
+assert account.username == "hakancelik"
+
+# Account(first_name="Hakan", last_name="Çelik", username=1)
